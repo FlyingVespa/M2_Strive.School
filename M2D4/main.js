c6e6fa521922366d2a52772c2167b0993b7800a6
Helena Nel <mailhedri@gmail.com>
2021-03-25 23:49:43 +0100

    a
    a

--- M2D4/main.js
+++ M2D4/main.js
@@ -3,18 +3,6 @@
 //   let pergroup = document.querySelectorAll(".pergroup");
 //   pergroup = parseInt(pergroup).value;
 
-//   let allnames = document.querySelector("textarea");
-//   allnames = allnames.value;
-//   allnames = allnames.split("\n");
-//   allnameslen = allnames.length;
-
-//   let numgroups = Math.ceil(allnameslen / namespergroup);
-
-//   if (numgroups.value > 1) {
-//     numgroups = parseInt(numgroups.value);
-//     namespergroup = allnameslen / numgroups;
-//   }
-
 //   document.getElementsByClassName("groups").empty();
 
 //   let groups = document.querySelectorAll(".groups");
@@ -63,6 +51,7 @@
 //   $(".pergroup-wrap, .numgroups-wrap").find("input").val("");
 // });
 
+//Add person to main List
 function addPerson() {
   let ul = document.getElementById("person-main-list");
   let person = document.getElementById("person");
@@ -71,17 +60,61 @@ function addPerson() {
   li.appendChild(document.createTextNode(person.value));
   ul.appendChild(li);
 }
-
+// Delete Name from Main List
 function removePerson() {
   let ul = document.getElementById("person-main-list");
-  let person = document.getElementById("person");
-  let item = document.getElementById(person.value);
-  ul.removeChild(item);
+  let person = document.getElementById("personlisted");
+  ul.removeChild(person);
 }
 
-const getTotalPeople = () => {
+// Get The total People on Main List (before assigning to groups)
+function getTotalPeople() {
   let person = document.querySelectorAll("#personlisted").length;
-  let count = document.querySelector(".tott");
+  let count = document.querySelector(".total-person-mainlist");
   count.innerText = person + " People On List";
   console.log(person);
-};
+}
+
+// Gets Amount of Groups
+function groupTotal() {
+  let NumberOfGroups = document.getElementById("group-amount").value;
+  let NumberOfGroup = document.querySelector(".group-amount-show");
+  NumberOfGroup.innerText = NumberOfGroups;
+  console.log(NumberOfGroups);
+}
+
+// Return Name To Main List
+function returnNameToMainList() {
+  let personName = document.getElementsByName("personName");
+  document.getElementById("person-main-list").appendChild(personName);
+}
+
+//Assign Person to Group
+
+//create array off personlist
+var x = 0;
+var array = Array();
+
+function add_element_to_array() {
+  array[x] = document.getElementById("person").value;
+
+  x++;
+  document.getElementById("person").value = "";
+}
+
+function display_array() {
+  var e = "<hr/>";
+
+  for (var y = 0; y < array.length; y++) {
+    e += "Element " + y + " = " + array[y] + "<br/>";
+  }
+  document.getElementById("Result").innerHTML = e;
+}
+
+//Shuffle array
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
